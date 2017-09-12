81b7a84ada20af5af94e99856b4402ec2fcca57b
MURILO DE SIQUEIRA VASCONCELOS <msv2079@gmail.com>
2017-09-12 00:37:14 -0300

    atualização de plugin
    ajustes para trabalhar apenas com uma coluna com descrição e para setar tamanho do modal e da imagem

--- msvfancybox/msvfancybox.js
+++ msvfancybox/msvfancybox.js
@@ -1,44 +1,70 @@
-$(function()
+$(function ()
 {
-	$(".fancybox").click(function(e)
-	{
-		e.preventDefault();
+    $(".fancybox").click(function (e)
+    {
+        e.preventDefault();
 
-		$("#ModalFancyBoxTitle").empty();
-		$("#ModalFancyBoxTextLeft").empty();
-		$("#ModalFancyBoxTextRight").empty();
-		$("#ModalFancyBoxImage").attr("src", "");
-		
-		$("#ModalFancyBoxTitle").html($(this).data("title"));
-		$("#ModalFancyBoxTextLeft").html($(this).data("textleft"));
-		$("#ModalFancyBoxTextRight").html($(this).data("textright"));
-		$("#ModalFancyBoxImage").attr("src", $("img", this).attr("src"));
-					
-		$("html").css("overflow", "hidden");
-		$("#BGModalFancyBox").show();
-		$("#ModalFancyBox").show();
-								
-		var widthLeft = ((($("img", this).prop('naturalWidth') + 40) / 2) * -1) + "px";	
-		var offsetTop = $("#mainmenu").offset().top;
-		var widthTop = "15%";
-		console.log(offsetTop);
-		
-		if (offsetTop != 0)
-		{
-			widthTop = (offsetTop + 100) + "px";	
-		}
-		
-		$("#ModalFancyBox").css("left", widthLeft);
-		$("#ModalFancyBox").css("margin-top", widthTop);
-		
-	});
-	
-	$("#ModalFancyBoxClose").click(function(e)
-	{
-		e.preventDefault();
-		
-		$("html").css("overflow", "scroll");
-		$("#BGModalFancyBox").hide();
-		$("#ModalFancyBox").hide();
-	});
+        $("#ModalFancyBoxTitle").empty();
+        $("#ModalFancyBoxTextLeft").empty();
+        $("#ModalFancyBoxTextRight").empty();
+        $("#ModalFancyBoxImage").attr("src", "");
+
+        $("#ModalFancyBoxTitle").html($(this).data("title"));
+        $("#ModalFancyBoxTextLeft").html($(this).data("textleft"));
+
+        if ($(this).data("textright") == "" || $(this).data("textright") == undefined)
+        {
+            $("#ModalFancyBoxBodyLeft").css({"width": "100%", "border": "none"});
+            $("#ModalFancyBoxBodyRight").remove();
+        } else
+        {
+            $("#ModalFancyBoxTextRight").html($(this).data("textright"));
+        }
+
+        $("#ModalFancyBoxImage").attr("src", $("img", this).attr("src"));
+        
+        if ($(this).data("imageheight") != undefined && $(this).data("imageheight") != "")
+        {
+            $("#ModalFancyBoxImage").css("height", $(this).data("imageheight") + "px");
+        }
+
+        $("html").css("overflow", "hidden");
+
+        var widthModal = $("img", this).prop('naturalWidth');
+
+        if ($(this).data("width") != undefined && $(this).data("width") != "")
+        {
+            widthModal = $(this).data("width");
+        }
+
+        $("#ModalFancyBox").css("width", widthModal + "px");
+
+        var widthLeft = (((widthModal + 40) / 2) * -1) + "px";
+        console.log(widthLeft);
+        var offsetTop = $("#mainmenu").offset().top;
+        var widthTop = "15%";
+
+        if (offsetTop != 0)
+        {
+            widthTop = (offsetTop + 50) + "px";
+        }
+
+        $("#ModalFancyBox").css(
+                {
+                    "left": widthLeft,
+                    "margin-top": widthTop
+                });
+                
+        $("#BGModalFancyBox").show();
+        $("#ModalFancyBox").show();
+    });
+
+    $("#ModalFancyBoxClose").click(function (e)
+    {
+        e.preventDefault();
+
+        $("html").css("overflow", "scroll");
+        $("#BGModalFancyBox").hide();
+        $("#ModalFancyBox").hide();
+    });
 });
